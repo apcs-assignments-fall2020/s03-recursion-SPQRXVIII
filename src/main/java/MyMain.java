5eaf831d28d0b2b9393588cc942cd825836f7b7d
SPQRXVIII <Aftersilver001@gmail.com>
2021-02-04 18:18:40 +0000

    good

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,8 +2,12 @@ public class MyMain {
     // This method adds up the values of all digits
     // in x, recursively
     public static int addDigits(int x) { 
-        // YOUR CODE HERE
-        return -1;
+        if(x%10==x){
+            return x;
+        }
+        else{
+            return x%10 + addDigits((x-x%10)/10);
+        }
     }
 
 
@@ -11,17 +15,35 @@ public class MyMain {
     // (e.g. "racecar", "madam"), recursively
     public static boolean isPalindrome(String str) { 
         // YOUR CODE HERE
-        return false;
+        int l = str.length();
+        if(l>1){
+            boolean a = str.charAt(0)==str.charAt(l-1);
+            return a&&isPalindrome(str.substring(1, l-1));
+        }
+        else{
+            return true;
+        }
+        
     }
 
     // This method returns the orignal string reversed;
     // this method should be written using recursion
     public static String reverse(String str) { 
         // YOUR CODE HERE
-        return "";
+        if(str.length()>1){
+            return str.charAt(str.length()-1)+reverse(str.substring(0, str.length()-1));
+        }
+        else{
+            return str;
+        }
     }
 
     public static void main(String[] args) {
         // You can test your code here
+        System.out.println(addDigits(123));
+        System.out.println(reverse("str"));
+        if(isPalindrome("aba")){
+            System.out.println("aba");
+        }
     }
 }
